a8c4b14da2edf4f3ba6c44bb05cbb90e15927eaa
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-18 12:59:33 -0500

    clean up sections first

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,16 +5,16 @@ gsap.registerPlugin(GSDevTools);
 // import { DrawSVGPlugin } from "./sections/logosection/logo.js";
 // gsap.registerPlugin(DrawSVGPlugin);
 
-import {Section2Animation} from "./sections/section-2/section2.js"
-import {Section3Animation} from "./sections/section-3/section3.js"
+// import {Section2Animation} from "./sections/section-2/section2.js"
+// import {Section3Animation} from "./sections/section-3/section3.js"
 import {Section4Animation} from "./sections/section-4/section4.js"
 
 
 
 
 const mainTL = gsap.timeline();
-mainTL.add(Section2Animation());
-mainTL.add(Section3Animation());
+// // mainTL.add(Section2Animation());
+// mainTL.add(Section3Animation());
 mainTL.add(Section4Animation());
 
 
--- dev/js/sections/section-2/section2.js
+++ dev/js/sections/section-2/section2.js
@@ -1,55 +1,55 @@
-import {gsap} from "gsap";
-import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
-gsap.registerPlugin(DrawSVGPlugin);
+// import {gsap} from "gsap";
+// import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+// gsap.registerPlugin(DrawSVGPlugin);
 
-// import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
-// gsap.registerPlugin(ScrambleTextPlugin);
+// // import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+// // gsap.registerPlugin(ScrambleTextPlugin);
 
-// gsap.set("#quotations",{transformOrigin: "center"});
-// gsap.set("#section2quotations",{transformOrigin: "center"});
+// // gsap.set("#quotations",{transformOrigin: "center"});
+// // gsap.set("#section2quotations",{transformOrigin: "center"});
 
-export function Section2Animation() {
-    var section2tl = gsap.timeline();
+// export function Section2Animation() {
+//     var section2tl = gsap.timeline();
 
-    section2tl.from("#quotation11section2, #quotation18section2, #quotation17section2, #quotation8section2, #quotation10section2", {
-        x:90,
-        scale: 4,
-        duration: 2,
-        alpha: 1
+//     section2tl.from("#quotation11section2, #quotation18section2, #quotation17section2, #quotation8section2, #quotation10section2", {
+//         x:90,
+//         scale: 4,
+//         duration: 2,
+//         alpha: 1
         
-    },"hey");
+//     },"hey");
 
 
-    section2tl.from("#quotation4section2, #quotation6section2, #quotation7section2, #quotation9section2, #quotation1section2", {
-        x:0,
-        y:200,
-        scale: 4,
-        duration: 2
-        
-
-    },"hey");
-
-    section2tl.from("#quotation16section2, #quotation15section2, #quotation5section2, #quotation2section2, #quotation14section2", {
-        x:0,
-        y:0,
-        scale: 4,
-        duration: 2
-
+//     section2tl.from("#quotation4section2, #quotation6section2, #quotation7section2, #quotation9section2, #quotation1section2", {
+//         x:0,
+//         y:200,
+//         scale: 4,
+//         duration: 2
         
 
-    },"hey");
+//     },"hey");
 
-    section2tl.from("#quotation3section2, #quotation13section2, #quotation12section2", {
-        y:120,
-        x:180,
-        scale: 4,
-        duration: 2
+//     section2tl.from("#quotation16section2, #quotation15section2, #quotation5section2, #quotation2section2, #quotation14section2", {
+//         x:0,
+//         y:0,
+//         scale: 4,
+//         duration: 2
+
         
 
-    },"hey");
+//     },"hey");
+
+//     section2tl.from("#quotation3section2, #quotation13section2, #quotation12section2", {
+//         y:120,
+//         x:180,
+//         scale: 4,
+//         duration: 2
+        
+
+//     },"hey");
 
 
 
 
-    return section2tl;
-}
+//     return section2tl;
+// }
