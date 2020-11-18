dff02e2247e4f50b0b2063e70e4a7f6da08d620a
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-18 15:14:46 -0500

    trying somethings

--- dev/js/sections/section-3/section3.js
+++ dev/js/sections/section-3/section3.js
@@ -1,22 +1,42 @@
-import {gsap} from "gsap";
-import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
-gsap.registerPlugin(DrawSVGPlugin);
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin, MotionPathHelper,MotionPathPlugin);
 
 // import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 // gsap.registerPlugin(ScrambleTextPlugin);
 
-gsap.set("#quotations",{transformOrigin: "center"});
+// gsap.set("#section3quotations",{transformOrigin: "center"});
 
 export function Section3Animation() {
     var section3tl = gsap.timeline();
 
-    section3tl.from("#quotation1", {
-        x:20,
-        scale: -20,
-        duration: 3
+    section3tl.from("#Fill-26", {
+        duration: 1,
+        scale: 10
+        // motionPath:{
+        //     Path:"#Path6section3path",
+        //     align:"#Path6section3path"
+        // } 
+
+    },"speed1");
+
+    // section3tl.from("#Fill-14", {
+    //     duration: 1,
+    //     scale: 10
+    //     // y:1000,
+    //     // startAt:{y:200}
+    //     // // x:500,
+    //     // // startAt:{X:-20}
+    //     // // ease: "back.out(1.7)",
+    //     // // stagger:1
         
 
-    },"hey");
+    // },"speed1");
+
 
 
 
